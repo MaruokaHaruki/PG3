531b712d582aa124125e82c5243bf763a209eb74
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2024-11-11 12:40:02 +0900

    11/11 ・クラステンプレートを作成しました

--- main.cpp
+++ main.cpp
@@ -9,23 +9,41 @@
 #include <stdio.h>
 #include <iostream>
 #include <Windows.h>
+#include "Math.h"
 
  ///=============================================================================
  ///						MAIN
 int main() {
 	SetConsoleOutputCP(65001);
-	printf("HELLOhello");
-	printf("丸岡春稀ですよ");
 
 	///--------------------------------------------------------------
 	///						 変数宣言・初期化
+	// クラステンプレートのインスタンス化
+	// int 型に対するインスタンス
+	Math<int> intMath;
 
+	// float 型に対するインスタンス
+	Math<float> floatMath;
+
+	// double 型に対するインスタンス
+	Math<double> doubleMath;
 
 
 
 	///--------------------------------------------------------------
 	///							処理
+	int a = 5;
+	int b = 10;
 
+	float c = 5.5f;
+	float d = 10.5f;
+
+	double e = 5.55;
+	double f = 10.55;
+
+	printf("%d と %d だと %d　のほうが小さい\n", a, b, intMath.Min(a, b));
+	printf("%.2f と %.2f だと %.2f　のほうが小さい\n", c, d, floatMath.Min(c, d));
+	printf("%.2lf と %.2lf だと %.2lf　のほうが小さい\n", e, f, doubleMath.Min(e, f));
 
 	return 0;
 }
