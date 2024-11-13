2d80ed82cd097e6f290c76429e19f6594b53292d
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2024-11-13 23:48:26 +0900

    11/13 ・ポリモーフィズムを実装しました

--- main.cpp
+++ main.cpp
@@ -9,23 +9,35 @@
 #include <stdio.h>
 #include <iostream>
 #include <Windows.h>
+#include "Instrument.h"
+#include "Piano.h"
+#include "Guitar.h"
+#include <vector>
+
+
 
  ///=============================================================================
  ///						MAIN
 int main() {
 	SetConsoleOutputCP(65001);
-	printf("HELLOhello");
-	printf("丸岡春稀ですよ");
 
 	///--------------------------------------------------------------
 	///						 変数宣言・初期化
-
+	// 楽器のインスタンスをポリモーフィズムで管理
+	std::vector<std::unique_ptr<Instrument>> instruments;
+	instruments.push_back(std::make_unique<Piano>());
+	instruments.push_back(std::make_unique<Guitar>());
 
 
 
 	///--------------------------------------------------------------
 	///							処理
 
+	// 各楽器の音を鳴らす
+	// Note:auto& instrument : instrumentsは範囲ベースfor文。正直、明示的にすべきではある。
+	for(const auto& instrument : instruments) {
+		instrument->playSound();
+	}
 
 	return 0;
 }
